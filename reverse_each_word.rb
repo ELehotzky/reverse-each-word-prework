7a283c5ca814a5385103df5d82486cf298a99c10
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-07-09 00:52:44 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+def reverse_each_word(sentence)
+  array = sentence.split()
+  reversed_array = []
+  array.collect { |word| reversed_array << word.reverse }
+  reversed_array
+  reversed_array.join(" ")
+end
