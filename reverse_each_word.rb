84a0e9c51571de4fd65f1fd991570431c4299ec5
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-07-09 00:52:10 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,12 +6,3 @@ def reverse_each_word(sentence)
   reversed_array.join(" ")
   end
 end
-
-
-
- reverse_each_word(sentence)
-+  array = sentence.split()
-+  new_array = [ ]
-+  array.collect { |word| new_array << word.reverse }
-+  new_array
-+  new_array.join(" ")
